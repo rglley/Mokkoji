38309b338915a0882ad28eeb679bc74ea8d7da98
김지은 <jieun9912@gmail.com>
2024-01-18 17:22:50 +0900

    feat : 백엔드 프로젝트 init
    - 1. 백엔드 초기 환경 셋팅
    - 더 해야할 것이 남았음(querydsl, redis 등)

---
+++ backend/mokkoji/src/main/java/online/mokkoji/MokkojiApplication.java
@@ -0,0 +1,13 @@
+package online.mokkoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokkojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MokkojiApplication.class, args);
+    }
+
+}
