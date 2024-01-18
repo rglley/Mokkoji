27a77944598c1f8937e28b36e4c49016f528f07b
cyy8071 <cyy8071@gmail.com>
2024-01-18 16:52:33 +0900

    feat : 진짜 마지막 fe 스켈레톤 - 1. fe 스켈레톤 - 2. 브랜치 작업 끝

---
+++ front-skeleton/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header class="flex items-center justify-between px-4 lg:px-6 h-30">
+      <TheHeader />
+    </header>
+    <Router-view/>
+  </div>
+</template>
+
+<script setup>
+import TheHeader from './components/common/TheHeader.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ front-skeleton/src/components/mypage/MyPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <img src="@/assets/dummy_profile.jpg" class="h-50">
+    </div>
+    <div>
+        
+        <p>{{ store.user.id }}님</p>
+        <div v-if="store.user.isAccountLinked">
+            계좌 연결됨
+        </div>
+        <div v-else>
+            <p>계좌를 연결하면 ____ </p>
+            <router-link to="#">계좌 연결</router-link>
+
+        </div>
+        <div>
+            <!-- 행사 기록 시각화 -->
+            
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useUserStore } from '../../stores/user';
+
+const store = useUserStore();
+
+// 로딩 시 
+onMounted(() => {
+    // console.log(store.user.email)
+})
+
+console
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
