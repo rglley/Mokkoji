592be2e88d9571090de0851082959470615d40b1
이경배 <foofoo964@naver.com>
2024-01-20 22:05:03 +0900

    [S10P12A401-94] feat : SecurityConfig 추가
    - 1. /users 요청은 USER 권한 확인, 정적 파일들, h2-console 요청은 모두
      허용
    - 2. 로그인이 필요한 요청들은 권한 확인이 필요하다

---
+++ backend/mokkoji/src/main/java/online/mokkoji/config/auth/SecurityConfig.java
@@ -0,0 +1,48 @@
+package online.mokkoji.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import online.mokkoji.db.entity.User;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                //h2-console을 사용하기 위한 설정
+                //https://dukcode.github.io/spring/h2-console-with-spring-security/
+                .csrf(csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console()))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+
+                //authorizeRequests : URL별 권한 관리 설정의 시작점
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                //permitAll 회의 참여하기 추가 필요
+                                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                                .requestMatchers("/users").hasRole("USER")
+                                //.requestMatchers("/admin").hasRole("ADMIN")
+                                //.anyRequest().authenticated() authenticated : 로그인된
+                )
+
+                //logout 성공 시 설정 주소로 이동
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+
+                //OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 customOAuth2UserService를 사용
+                .oauth2Login(oauth2Login ->
+                        oauth2Login.userInfoEndpoint(userInfoEndpointConfig ->
+                                userInfoEndpointConfig.userService(customOAuth2UserService)
+                        )
+                );
+
+        return http.build();
+    }
+}
