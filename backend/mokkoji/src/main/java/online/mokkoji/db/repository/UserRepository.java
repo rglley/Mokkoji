8825e537ad56cea6c41118aa377362664bb425d4
이경배 <foofoo964@naver.com>
2024-01-20 14:34:06 +0900

    [S10P12A401-94] feat : User repository 생성

---
+++ backend/mokkoji/src/main/java/online/mokkoji/db/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package online.mokkoji.db.repository;
+
+import online.mokkoji.db.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
