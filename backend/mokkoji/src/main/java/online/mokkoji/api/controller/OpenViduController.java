06c90a676c31e19ff9a564b211a450a1465c3c19
김지은 <jieun9912@gmail.com>
2024-01-23 08:56:39 +0900

    chore : remove ignored file

--- backend/mokkoji/src/main/java/online/mokkoji/api/controller/OpenViduController.java
+++ backend/mokkoji/src/main/java/online/mokkoji/api/controller/OpenViduController.java
@@ -1,4 +1,11 @@
 package online.mokkoji.api.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class OpenViduController {
+
+
 }
