9dc5483b6c292331dd03bc407d7c85da54c679ff
이경배 <foofoo964@naver.com>
2024-01-26 17:25:43 +0900

    refactor : 에러코드 HttpStatus에서  Integer로 변경
    Change-Id: I986eb4487e52576e81f8305e3731cd679fea7b64

--- backend/mokkoji/src/main/java/online/mokkoji/common/exception/errorCode/ErrorCode.java
+++ backend/mokkoji/src/main/java/online/mokkoji/common/exception/errorCode/ErrorCode.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 //에러 코드 정의 추상화를 위한 인터페이스
 public interface ErrorCode {
     String name();
-    HttpStatus getHttpStatus();
-    String getMessage();
+    Integer getErrorCode();
+    String getErrorMessage();
 }
