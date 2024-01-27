ef732867fcdc38225eec6baf13721c4ed749474a
김지은 <jieun9912@gmail.com>
2024-01-27 14:32:20 +0900

    fix : handler에 에러코드 get 메서드들 수정, application.properties를 yml로 변환
    - 1. handler 메서드 수정
    - 2. 바꾼 메서드들에 맞춰 업데이트가 안되어 있어 수정함
    -
    - 1. yml에 전부 넣으면 보기 좋을 것 같아 변환 후 수정
    
    Change-Id: Ie56677f99bd5027a006b09074cd7e00fdb029f84

--- backend/mokkoji/src/main/java/online/mokkoji/common/exception/handler/GlobalExceptionHandler.java
+++ backend/mokkoji/src/main/java/online/mokkoji/common/exception/handler/GlobalExceptionHandler.java
@@ -5,26 +5,16 @@ import online.mokkoji.common.exception.RestApiException;
 import online.mokkoji.common.exception.errorCode.CommonErrorCode;
 import online.mokkoji.common.exception.errorCode.ErrorCode;
 import online.mokkoji.common.exception.response.ErrorResponse;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 //전역적으로 에러를 처리해주는 클래스
@@ -50,6 +40,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
         return handleExceptionInternal(errorCode);
     }
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
@@ -69,19 +60,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
-        return ResponseEntity.status(errorCode.getHttpStatus())
+        return ResponseEntity.status(errorCode.getErrorCode())
                 .body(makeErrorResponse(errorCode));
     }
 
     private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
         return ErrorResponse.builder()
                 .code(errorCode.name())
-                .message(errorCode.getMessage())
+                .message(errorCode.getErrorMessage())
                 .build();
     }
 
     private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
-        return ResponseEntity.status(errorCode.getHttpStatus())
+        return ResponseEntity.status(errorCode.getErrorCode())
                 .body(makeErrorResponse(errorCode, message));
     }
 
@@ -101,7 +92,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         return ErrorResponse.builder()
                 .code(errorCode.name())
-                .message(errorCode.getMessage())
+                .message(errorCode.getErrorMessage())
                 .errors(validationErrorList)
                 .build();
     }
