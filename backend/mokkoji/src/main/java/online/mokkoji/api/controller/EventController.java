456e8329798cc6d6b9f27b4e51e181b95c6dcb1f
김지은 <jieun9912@gmail.com>
2024-01-27 23:51:10 +0900

    feat : 사진 추가 메서드 컨트롤러에 추가
    - 1. photoReqDto에 사진, 유저아이디 담아서 사진 업로드하는 메서드 생성
    - 2. userId가 있어야 사진 분류가 가능해짐
    
    Change-Id: I5980e7c21166dae553f452643337a338c3968b3c

--- backend/mokkoji/src/main/java/online/mokkoji/api/controller/EventController.java
+++ backend/mokkoji/src/main/java/online/mokkoji/api/controller/EventController.java
@@ -2,14 +2,45 @@ package online.mokkoji.api.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import online.mokkoji.api.request.PhotoReqDto;
+import online.mokkoji.api.service.EventService;
+import online.mokkoji.api.service.S3ServiceImpl;
+import online.mokkoji.db.entity.Event.Event;
+import online.mokkoji.db.repository.EventRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/events")
 public class EventController {
 
+    private final EventRepository eventRepository;
+    private final EventService eventService;
+    private final S3ServiceImpl s3Service;
 
+    // 캡쳐사진 전송받음
+    @PostMapping("/photos/{sessionId}")
+    public ResponseEntity<Map<String, String>> addPhoto(@PathVariable("sessionId") String sessionId,
+                                                        @RequestBody PhotoReqDto photoReqDto) throws IOException {
+
+        Event event = eventRepository.findBySessionId(sessionId);
+        Long resultId = event.getResult().getId();
+        URL s3Url = s3Service.createImage(photoReqDto.getPhoto(), photoReqDto.getUserId(), resultId);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("Status", "사진 업로드 완료");
+
+        // redis에 url 저장
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
--- backend/mokkoji/src/main/java/online/mokkoji/api/request/PhotoReqDto.java
+++ backend/mokkoji/src/main/java/online/mokkoji/api/request/PhotoReqDto.java
@@ -1,2 +1,19 @@
-package online.mokkoji.api.request;public class PhotoReqDto {
+package online.mokkoji.api.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhotoReqDto {
+
+    @NotNull
+    private MultipartFile photo;
+    @NotNull
+    private Long userId;
+
 }
