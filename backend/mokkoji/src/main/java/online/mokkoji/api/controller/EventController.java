96c1d2ff45d2a70fe04410099d6af95cecdeeeaf
김지은 <jieun9912@gmail.com>
2024-01-27 14:15:39 +0900

    - 1. - 2.
    Change-Id: I8ac51ae8e62775943f6fa05e26f54a17930d1536

---
+++ backend/mokkoji/src/main/java/online/mokkoji/api/controller/EventController.java
@@ -0,0 +1,13 @@
+package online.mokkoji.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequiredArgsConstructor
+public class EventController {
+
+
+}
