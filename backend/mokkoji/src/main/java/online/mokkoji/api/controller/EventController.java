47c329f66efc0f1da214810be4dc74128355079e
김지은 <jieun9912@gmail.com>
2024-01-28 15:24:07 +0900

    feat : 롤링페이퍼 저장 controller method 생성
    - 1. 프론트에서 전체를 적지 않은 것은 거르고 하나라도 있는 경우로 생성,
      DTO 생성자로 requestBody 받아 옴
    - 2. 멀티 스레드에서도 생성자는 사용할 때 문제가 생기지 않는다고 한다
    
    Change-Id: I2276c608a1dd878e78b8e4544288ac1b3b09bbc9

--- backend/mokkoji/src/main/java/online/mokkoji/api/controller/EventController.java
+++ backend/mokkoji/src/main/java/online/mokkoji/api/controller/EventController.java
@@ -3,6 +3,7 @@ package online.mokkoji.api.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import online.mokkoji.api.request.PhotoReqDto;
+import online.mokkoji.api.request.RollingpaperReqDto;
 import online.mokkoji.api.service.EventService;
 import online.mokkoji.api.service.S3ServiceImpl;
 import online.mokkoji.db.entity.Event.Event;
@@ -10,6 +11,7 @@ import online.mokkoji.db.repository.EventRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URL;
@@ -27,20 +29,63 @@ public class EventController {
     private final EventService eventService;
     private final S3ServiceImpl s3Service;
 
-    // 캡쳐사진 전송받음
+    // 캡쳐사진 저장
     @PostMapping("/photos/{sessionId}")
     public ResponseEntity<Map<String, String>> addPhoto(@PathVariable("sessionId") String sessionId,
                                                         @RequestBody PhotoReqDto photoReqDto) throws IOException {
 
         Event event = eventRepository.findBySessionId(sessionId);
         Long resultId = event.getResult().getId();
-        URL s3Url = s3Service.createImage(photoReqDto.getPhoto(), photoReqDto.getUserId(), resultId);
+        URL s3Url = s3Service.uploadImage(photoReqDto.getPhoto(), photoReqDto.getUserId(), resultId);
 
         Map<String, String> response = new HashMap<>();
-        response.put("Status", "사진 업로드 완료");
+        response.put("message", "사진 업로드 완료");
 
-        // redis에 url 저장
+        // TODO: 2024.01.28 redis에 url 저장
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
+    //롤링페이퍼 저장
+    @PostMapping("/events/rollingpapers/{sessionId}")
+    public ResponseEntity<Map<String, String>> addRollingpaper(@PathVariable("sessionId") String sessionId,
+                                                               @RequestBody RollingpaperReqDto rollingpaperReqDto) throws IOException {
+
+        Event event = eventRepository.findBySessionId(sessionId);
+        Long resultId = event.getResult().getId();
+
+        Map<String, MultipartFile> fileMap = new HashMap<>();
+
+        String text = rollingpaperReqDto.getText();
+
+        if (!text.isEmpty() && text.length() > 0) {
+            // TODO : 2023.01.28 redis에 text 저장
+        }
+
+        // 음성이 있는 경우 map에 저장
+        MultipartFile voice = rollingpaperReqDto.getVoice();
+        if (voice != null && !voice.isEmpty()) {
+            fileMap.put("voice", voice);
+        }
+        // 영상이 있는 경우 map에 저장
+        MultipartFile video = rollingpaperReqDto.getVideo();
+        if (video != null && !video.isEmpty()) {
+            fileMap.put("video", video);
+        }
+
+        // 미디어 파일이 있는 경우만 uploadRollingpaper 실행
+        if (fileMap.size() > 0) {
+            Map<String, URL> urlMap = s3Service.uploadRollingpaper(fileMap, rollingpaperReqDto.getUserId(), resultId);
+
+            // TODO: 2024.01.28 redis에 저장
+        }
+
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "롤링페이퍼 업로드 완료");
+
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
