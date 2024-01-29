155eb745d6fd4b9b1079faafcf030d6ff6fc9427
김지은 <jieun9912@gmail.com>
2024-01-29 22:13:53 +0900

    [S10P12A401-333] feat : jwt로 유저Id 가져오게 변경
    - 1. jwt로 유저 Id 가져오기
    - 2. merge전까지는 jwt를 사용할 수 없었는데, 이제 가능
    
    Change-Id: Ibcc1237bc8789af13b17ad0c323aa54ed22a5e1e

--- backend/mokkoji/src/main/java/online/mokkoji/event/controller/EventController.java
+++ backend/mokkoji/src/main/java/online/mokkoji/event/controller/EventController.java
@@ -1,12 +1,16 @@
 package online.mokkoji.event.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import online.mokkoji.S3.S3ServiceImpl;
-import online.mokkoji.event.dto.request.PhotoReqDto;
+import online.mokkoji.common.auth.jwt.util.JwtUtil;
+import online.mokkoji.event.domain.Event;
 import online.mokkoji.event.dto.request.RollingpaperReqDto;
 import online.mokkoji.event.repository.EventRepository;
 import online.mokkoji.event.service.EventService;
+import online.mokkoji.user.domain.User;
+import online.mokkoji.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -25,56 +28,61 @@ import java.util.Map;
 public class EventController {
 
     private final EventRepository eventRepository;
+    private final UserService userService;
     private final EventService eventService;
     private final S3ServiceImpl s3Service;
+    private final JwtUtil jwtUtil;
 
     // 캡쳐사진 저장
     @PostMapping("/photos/{sessionId}")
-    public ResponseEntity<?> addPhoto(@PathVariable("sessionId") String sessionId,
-                                      @RequestPart("photo") MultipartFile photo,
-                                      @RequestPart("userId") PhotoReqDto photoReqDto) throws IOException {
+    public ResponseEntity<String> addPhoto(@PathVariable("sessionId") String sessionId,
+                                           HttpServletRequest req,
+                                           @RequestPart("photo") MultipartFile photo) throws IOException {
 
-        // TODO : 2024.01.29 jwt에서 사용자 권한 받아오기
-//        Event event = eventRepository.findBySessionId(sessionId);
-//        Long resultId = event.getResult().getId();
-        Long resultId = 1L;
-        URL s3Url = s3Service.uploadImage(photo, photoReqDto.getUserId(), resultId);
+        String provider = jwtUtil.getProvider(req);
+        String email = jwtUtil.getEmail(req);
+
+        User user = userService.getByProviderAndEmail(provider, email);
+
+        Event event = eventRepository.findBySessionId(sessionId);
+        Long resultId = event.getResult().getId();
+        URL s3Url = s3Service.uploadImage(photo, user.getId(), resultId);
 
         log.info(s3Url.toString());
 
         // TODO: 2024.01.28 redis에 url 저장
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>("사진 업로드 완료", HttpStatus.OK);
     }
 
     //롤링페이퍼 저장
     @PostMapping("/rollingpapers/{sessionId}")
-    public ResponseEntity<Map<String, ?>> addRollingpaper(@PathVariable("sessionId") String sessionId,
-                                                          @RequestPart(value = "voice", required = false) MultipartFile voice,
-                                                          @RequestPart(value = "video", required = false) MultipartFile video,
-                                                          @RequestPart("dto") RollingpaperReqDto rollingpaperReqDto) throws IOException {
+    public ResponseEntity<String> addRollingpaper(@PathVariable("sessionId") String sessionId,
+                                                  HttpServletRequest req,
+                                                  @RequestPart(value = "voice", required = false) MultipartFile voice,
+                                                  @RequestPart(value = "video", required = false) MultipartFile video,
+                                                  @RequestPart("dto") RollingpaperReqDto rollingpaperReqDto) throws IOException {
 
-        // TODO : 2024.01.29 jwt에서 사용자 정보 받기
-//        Event event = eventRepository.findBySessionId(sessionId);
-//        Long resultId = event.getResult().getId();
-        Long resultId = 1L;
+        String provider = jwtUtil.getProvider(req);
+        String email = jwtUtil.getEmail(req);
+
+        User user = userService.getByProviderAndEmail(provider, email);
+        Event event = eventRepository.findBySessionId(sessionId);
+        Long resultId = event.getResult().getId();
 
         rollingpaperReqDto.setVoice(voice);
         rollingpaperReqDto.setVideo(video);
 
         // 텍스트, 음성, 영상 유효성 검사 후 file, text 맵에 담음
-        Map<String, Map> rollingpaperMap = eventService.createRollingpaperFileMap(rollingpaperReqDto);
+        Map<String, MultipartFile> fileMap = eventService.createRollingpaperFileMap(rollingpaperReqDto);
 
 
-        Map<String, MultipartFile> fileMap = rollingpaperMap.get("files");
         // 유효성 검사 후 파일 S3에 업로드
-        Map<String, URL> urlMap = s3Service.uploadRollingpaper(fileMap, rollingpaperReqDto.getUserId(), resultId);
+        Map<String, URL> urlMap = s3Service.uploadRollingpaper(fileMap, user.getId(), resultId);
 
-        Map<String, Map> response = new HashMap<>();
-        response.put("text", rollingpaperMap.get("text"));
-        response.put("fileURL", urlMap);
+        // TODO : redis에 전부 저장
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>("롤링페이퍼 업로드 완료", HttpStatus.OK);
 
     }
 
--- backend/mokkoji/src/main/java/online/mokkoji/openvidu/controller/OpenviduController.java
+++ backend/mokkoji/src/main/java/online/mokkoji/openvidu/controller/OpenviduController.java
@@ -2,11 +2,15 @@ package online.mokkoji.openvidu.controller;
 
 import io.openvidu.java.client.*;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import online.mokkoji.common.auth.jwt.util.JwtUtil;
 import online.mokkoji.openvidu.dto.request.SessionReqDto;
 import online.mokkoji.event.service.EventService;
 import online.mokkoji.event.repository.EventRepository;
+import online.mokkoji.user.domain.User;
+import online.mokkoji.user.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +27,8 @@ public class OpenviduController {
 
     private final EventService eventService;
     private final EventRepository eventRepository;
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
 
 
     @Value("${OPENVIDU_URL}")
@@ -40,8 +46,11 @@ public class OpenviduController {
 
     // Session 생성
     @PostMapping("/api/sessions")
-    public ResponseEntity<Map<String, String>> addSession(@RequestBody(required = false) Map<String, Object> params)
+    public ResponseEntity<Map<String, String>> addSession(@RequestBody(required = false) Map<String, Object> params,
+                                                          HttpServletRequest req)
             throws OpenViduJavaClientException, OpenViduHttpException {
+
+        User user = userService.getByProviderAndEmail(jwtUtil.getProvider(req), jwtUtil.getEmail(req));
 
         // request body 객체로 직렬화
         SessionProperties properties = SessionProperties.fromJson(params).build();
@@ -49,13 +58,8 @@ public class OpenviduController {
         //세션 생성
         Session session = openvidu.createSession(properties);
 
-
-        // DB에 담을 유저Id를 받기
-        Long userId = eventService.getUserId(params);
-
-
         // DB에 저장할 Dto 생성
-        SessionReqDto sessionReqDto = new SessionReqDto(userId, session.getSessionId(), session.createdAt());
+        SessionReqDto sessionReqDto = new SessionReqDto(user.getId(), session.getSessionId(), session.createdAt());
 
         // DB에 저장
         String sessionDBId = eventService.createSession(sessionReqDto);
@@ -72,9 +76,13 @@ public class OpenviduController {
 
     // Session 삭제
     @DeleteMapping("/api/sessions/{sessionId}")
-    public ResponseEntity<?> deleteSession(@PathVariable("sessionId") String sessionId,
-                                           @RequestBody(required = false) SessionReqDto sessionReqDto)
+    public ResponseEntity<String> deleteSession(@PathVariable("sessionId") String sessionId,
+                                                HttpServletRequest req,
+                                                @RequestBody(required = false) SessionReqDto sessionReqDto)
             throws OpenViduJavaClientException, OpenViduHttpException {
+
+        User user = userService.getByProviderAndEmail(jwtUtil.getProvider(req), jwtUtil.getEmail(req));
+        sessionReqDto.setUserId(user.getId());
 
         Session activeSession = openvidu.getActiveSession(sessionId);
         eventService.deleteSession(sessionId, sessionReqDto);
@@ -85,7 +93,7 @@ public class OpenviduController {
         // TODO: 2024.01.28 redis에 남은 결과물 파일 저장(resultService)||아직 작성 중인 사람은?
 
 
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>("삭제 완료", HttpStatus.NO_CONTENT);
     }
 
 
