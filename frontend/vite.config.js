8eeab5449df5e88bc1d0f4ce527d0ec51a836193
박철준 <dizncjfwns@nate.com>
2024-02-01 16:38:21 +0900

    fix : vite.config.js에 css 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  css: {
+    preprocessorOptions: {}
+  },
   optimizeDeps: {
     include: ['events']
   }
