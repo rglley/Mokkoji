709e3bd7a5dd24990c79b19576f0351a837c0646
김지은 <jieun9912@gmail.com>
2024-02-05 09:52:23 +0900

    chore : 프론트 폴다더 삭제
    Change-Id: I7a73052a4716c02c12bf8c8d28d5987567e96c2f

--- frontend/src/api/get_images.js
+++
@@ -1,8 +0,0 @@
-const getImages = (number) => {
-  let photos = []
-  for (var i = 1; i <= number; i++) {
-    photos.push(`src/assets/memory/memory_random_${i}.png`)
-  }
-  return photos
-}
-export default getImages
