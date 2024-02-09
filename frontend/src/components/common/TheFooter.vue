689cdc6265b09c4fc3f5b51a16b5d4c8b01fd911
HunTeac <98cline@naver.com>
2024-02-09 22:40:55 +0900

    feat : 홈 화면 1. 레이아웃 수치 수정

--- frontend/src/components/common/TheFooter.vue
+++ frontend/src/components/common/TheFooter.vue
@@ -1,9 +1,9 @@
 <template>
   <footer class="bg-slate-800">
     <hr class="border-gray-200" />
-    <div class="w-full max-w-screen-xl mx-auto p-4 h-auto">
-      <hr class="border-white mt-10" />
-      <span class="block text-white h-12 mh-2"
+    <div class="w-full max-w-screen-xl mx-auto p-[2vh] h-auto">
+      <hr class="border-white mt-[2vw] w-[100%]" />
+      <span class="block text-white text-r-md h-[3vw]"
         >© <span class="hover:underline text-white">모꼬지™ All rights reserved.</span>
       </span>
     </div>
--- frontend/src/components/common/TheHeader.vue
+++ frontend/src/components/common/TheHeader.vue
@@ -1,23 +1,23 @@
 <template>
   <header
     :class="{ 'transparent-header': isTransparent }"
-    class="fixed h-32 z-10 my-auto w-full bg-transparent"
+    class="fixed h-[10vh] z-10 my-auto w-full bg-transparent"
   >
     <nav class="w-full flex px-[2vw]">
-      <router-link to="/" class="h-28 flex items-center rtl:l:space-x-reverse">
+      <router-link to="/" class="h-[10vh] flex items-center rtl:l:space-x-reverse">
         <img
           src="/src/assets/logo/mokkoji_logo.png"
-          class="h-24 mx-5 transition ease-in-out hover:animate-pulse"
+          class="h-[10vh] transition ease-in-out hover:animate-pulse"
           alt="모꼬지 로고"
         />
         <!-- <span id="title-bold">모꼬지</span> -->
       </router-link>
 
-      <div class="ml-auto mr-1 self-center">
+      <div class="ml-auto self-center">
         <ul class="font-medium flex md:flex-row ml-10">
-          <button id="button-header"><a href="/">홈으로</a></button>
+          <button id="button-header"><a href="/" class="text-[2vh]">홈으로</a></button>
           <li v-if="!(store.isLogin || isLogin)">
-            <button id="button-header" @click="showLoginModal">로그인</button>
+            <button id="button-header" @click="showLoginModal" class="text-[2vh]">로그인</button>
             <ModalView v-if="isLoginModal" :show-modal="isLoginModal" @close-modal="showLoginModal">
               <LoginModal />
             </ModalView>
@@ -33,20 +33,20 @@
               </button>
               <div class="flex justify-center items-center gap-2 rounded-full mx-2">
                 <img class="overflow-hidden rounded-full w-12 m-0;" :src="image" />
-                <p class="text-black text-lg">{{ name }}님</p>
+                <p class="text-black text-[2vh]">{{ name }}님</p>
               </div>
               <div
                 id="dropdown"
                 class="z-10 hidden bg-white divide-y divide-slate-200 rounded-lg w-32"
               >
                 <ul aria-labelledby="dropdownHoverButton" class="w-50">
-                  <li id="li-dropdown">
+                  <li id="li-dropdown" class="text-[2vh]">
                     <router-link to="mypage">마이페이지</router-link>
                   </li>
-                  <li id="li-dropdown">
+                  <li id="li-dropdown" class="text-[2vh]">
                     <router-link to="eventlist">내 결과물</router-link>
                   </li>
-                  <li id="li-dropdown">
+                  <li id="li-dropdown" class="text-[2vh]">
                     <a @click="logout">로그아웃</a>
                   </li>
                 </ul>
