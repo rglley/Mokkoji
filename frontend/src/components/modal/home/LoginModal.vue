9a128b83cfc4a103e4d22d1a31483f65ad379bd0
cyy8071 <cyy8071@gmail.com>
2024-02-13 09:46:16 +0900

    fix : 로그인  url

--- frontend/src/components/modal/home/LoginModal.vue
+++ frontend/src/components/modal/home/LoginModal.vue
@@ -47,13 +47,13 @@
 </template>
 
 <script setup>
-const { VITE_NAVER_CLIENT_ID, VITE_NAVER_REDIRECT_URL } = import.meta.env
+const { VITE_NAVER_CLIENT_ID, VITE_NAVER_REDIRECT_URI } = import.meta.env
 
 const naverApiRequestURI =
   `https://nid.naver.com/oauth2.0/authorize?client_id=` +
   VITE_NAVER_CLIENT_ID +
   '&redirect_uri=' +
-  VITE_NAVER_REDIRECT_URL +
+  VITE_NAVER_REDIRECT_URI +
   '&state=mokkoji&response_type=code'
 </script>
 
